b2708c7fb5fe5d41271f00a36d6c83b317085cd7
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-07-18 14:35:41 -0300

    Done

---
+++ script.js
@@ -0,0 +1,47 @@
+//Crie 3 variáveis, cada uma com um array: Comedy, Action and Fantasy
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo: Title, director and year
+
+let Comedy = [{
+    title: 'Mean Girls',
+    director: 'Mark Waters',
+    year: 2004
+},{
+    title: '21 Jump Street',
+    director: 'Phil Lord and Chris Miller',
+    year: 2012
+},{
+    title: 'Encanto',
+    director: 'Byron Howards and Jared Bush',
+    year: 2021
+}]
+console.log(Comedy)
+
+let Action = [{
+    title: 'X men: First Class',
+    director: 'Matthew Vaughn',
+    year: 2011
+},{
+    title: 'Guardions of the Galaxy',
+    director: 'James Gunn',
+    year: 2014
+},{
+    title: 'Spider-Man: Into the Spider-Verse',
+    director: 'Peter Ramsey, Bob Persichetti and Rodney Rothman',
+    year: 2019
+}]
+console.log(Action)
+
+let Fantasy = [{
+    title: 'Hocus Pocus',
+    director: 'Kenny Ortega',
+    year: 1993
+},{
+    title: 'Charlie and the Chocolate Factory',
+    director: 'Tim Burton',
+    year: 2005,
+},{
+    title: 'Harry Potter and the Order of the Phoenix',
+    director: 'David Yates',
+    year:2007
+}]
+console.log(Fantasy)
